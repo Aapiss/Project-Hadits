db51a41fc52cf14958a5f7c7c6a8198473f40c2b
Aapiss <mhafidzmushtofa@gmail.com>
2023-10-31 09:17:20 +0700

    dark mode

--- main.js
+++ main.js
@@ -87,3 +87,35 @@ function btnSearch() {
       }
     })
 }
+
+// Fitur Dark Mode
+
+const toggleDarkMode = document.getElementById('btn-dark-mode');
+const theme = document.body;
+let darkMode = localStorage.getItem('dark-mode-theme');
+
+const enableDarkMode = () => {
+  theme.classList.add('dark-theme');
+  toggleDarkMode.classList.remove('dark-mode-toggle');
+  localStorage.setItem('dark-mode-theme', 'enable');
+};
+
+const disableDarkMode = () => {
+  theme.classList.remove('dark-theme');
+  toggleDarkMode.classList.add('dark-mode-toggle');
+  localStorage.setItem('dark-mode-theme', 'disable');
+};
+
+if (darkMode === 'enable') {
+  enableDarkMode();
+}
+
+toggleDarkMode.addEventListener('click', function () {
+  darkMode = localStorage.getItem('dark-mode-theme');
+
+  if (darkMode === 'disable') {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
